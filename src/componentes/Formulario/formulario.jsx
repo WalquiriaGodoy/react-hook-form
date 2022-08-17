4d1beae1b16c886c54ece2189cca2f8cbc399a2f
Walquiria Godoy <walquiriagodoy@gmail.com>
2022-08-17 16:53:07 -0300

    feat(form) change to function component

--- src/componentes/Formulario/formulario.jsx
+++ src/componentes/Formulario/formulario.jsx
@@ -1,36 +1,28 @@
 import React from 'react';
 import './formulario.css';
 
-class Formulario extends React.Component {
-  constructor() {
-    super();
-  }
+export default function Formulario() {
+  return (
+    <form>
+      <label>
+        Nome
+        <input />
+      </label>
 
-  render() {
-    return (
-      <form>
-        <label>
-          Nome
-          <input />
-        </label>
+      <label>
+        Email
+        <input />
+      </label>
 
-        <label>
-          Email
-          <input />
-        </label>
+      <label>
+        Cep
+        <input />
+      </label>
 
-        <label>
-          Cep
-          <input />
-        </label>
-
-        <label>
-          Endereço
-          <input />
-        </label>
-      </form>
-    );
-  }
+      <label>
+        Endereço
+        <input />
+      </label>
+    </form>
+  );
 }
-
-export default Formulario;
