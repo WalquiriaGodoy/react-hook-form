8d628456a68551b584a71dc3b8c18e9d2e949025
Walquiria Godoy <walquiriagodoy@gmail.com>
2022-08-17 16:48:24 -0300

    feat(names): ajust component names

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import Cabecalho from './componentes/cabecalho/cabecalho';
-import Formulario from './componentes/formulario/formulario';
+import Cabecalho from './componentes/Cabecalho/cabecalho';
+import Formulario from './componentes/Formulario/formulario';
 
 function App() {
   return (
